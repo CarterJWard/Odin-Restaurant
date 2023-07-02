155ff660ceb569cca271d6a3a346f0b48b3e0f26
Carter Ward <20886719+CarterJWard@users.noreply.github.com>
2023-07-02 19:06:11 +1000

    Fix wepback dev server Previous commit didn't actually install the webpack server. This is now fixed with config changes to match.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
+const App = () => {
+  return HomePage();
+};
 const HomePage = () => {
   let element = document.createElement("h1");
-  element.textContent = "Hello boomers";
+  element.textContent = "Welcome to Glorious Chicken";
+  return element;
+};
+
+const Menu = () => {
+  let element = document.createElement("h1");
+  element.textContent = "Menu";
+  element;
+};
+
+const About = () => {
+  let element = document.createElement("h1");
+  element.textContent = "About Us2";
   return element;
 };
 
