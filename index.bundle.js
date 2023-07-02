cef8ced1565252beb0ca88bfb9e4e0e153d075b0
Carter Ward <20886719+CarterJWard@users.noreply.github.com>
2023-07-02 18:18:30 +1000

    Implement webpack dev server Added webpack dev server and required config to enable auto reloading on save.

--- index.bundle.js
+++ index.bundle.js
@@ -1,1 +1,26 @@
-document.body.appendChild((()=>{let e=document.createElement("h1");return e.textContent="Hello Worlds",e})());
+(self["webpackChunkodin_restaurant"] = self["webpackChunkodin_restaurant"] || []).push([["index"],{
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+const HomePage = () => {
+  let element = document.createElement("h1");
+  element.textContent = "Hello boomers";
+  return element;
+};
+
+document.body.appendChild(HomePage());
+
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/index.js"));
+/******/ }
+]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7O0FBQUE7QUFDQTtBQUNBO0FBQ0E7QUFDQTs7QUFFQSIsInNvdXJjZXMiOlsid2VicGFjazovL29kaW4tcmVzdGF1cmFudC8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zdCBIb21lUGFnZSA9ICgpID0+IHtcbiAgbGV0IGVsZW1lbnQgPSBkb2N1bWVudC5jcmVhdGVFbGVtZW50KFwiaDFcIik7XG4gIGVsZW1lbnQudGV4dENvbnRlbnQgPSBcIkhlbGxvIGJvb21lcnNcIjtcbiAgcmV0dXJuIGVsZW1lbnQ7XG59O1xuXG5kb2N1bWVudC5ib2R5LmFwcGVuZENoaWxkKEhvbWVQYWdlKCkpO1xuIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
